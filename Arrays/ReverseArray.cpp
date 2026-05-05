fa925576ec7990775a07c19cc01e32bd4bd6a877
Hemanth Kumar S <venki009512@gmail.com>
2026-05-05 20:52:15 +0530

    Refactor ReverseArray.cpp for clarity and formatting
    Updated comments for clarity and improved code formatting.

--- Arrays/ReverseArray.cpp
+++ Arrays/ReverseArray.cpp
@@ -8,8 +8,8 @@ int main(){
     int end=n-1;
     while(st<end){
         swap(arr[st],arr[end]);
-        st++;
-        end--;
+        st++;    // Start Increment
+        end--;   //  End Decrement
     }
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
