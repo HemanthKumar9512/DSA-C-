b9d7051a5563dcf70a41ff2fcaf6c153b73c4b9f
Hemanth Kumar S <venki009512@gmail.com>
2026-07-09 19:00:00 +0530

    Delete OpenCV/Experiment1d.py

--- OpenCV/Experiment1d.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-img = cv2.imread('OpenCV/photo.jpg', 0)
-kernel = np.ones((5,5), np.uint8)
-dilated = cv2.dilate(img, kernel, iterations=1)
-cv2.imshow('Dilated', dilated)
-cv2.waitKey(0)
