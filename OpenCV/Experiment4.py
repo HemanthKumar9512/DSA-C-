ad5af8bfd61938a853b72ef88ec52955dde4848e
Hemanth Kumar S <venki009512@gmail.com>
2026-07-10 18:20:25 +0530

    Delete OpenCV/Experiment4.py

--- OpenCV/Experiment4.py
+++
@@ -1,7 +0,0 @@
-import cv2
-img = cv2.imread('OpenCV/photo.jpg')
-big = cv2.resize(img, None, fx=2, fy=2)
-small = cv2.resize(img, None, fx=0.5, fy=0.5)
-cv2.imshow('Bigger', big)
-cv2.imshow('Smaller', small)
-cv2.waitKey(0)
