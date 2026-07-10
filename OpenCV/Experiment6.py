8a56fabafa4698d289ebe793fd9c19f7d3f6a6a1
Hemanth Kumar S <venki009512@gmail.com>
2026-07-10 18:20:49 +0530

    Delete OpenCV/Experiment6.py

--- OpenCV/Experiment6.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-img = cv2.imread('OpenCV/photo.jpg')
-h, w = img.shape[:2]
-M = np.float32([[1, 0, 50], [0, 1, 100]])   # shift right 50, down 100
-moved = cv2.warpAffine(img, M, (w, h))
-cv2.imshow('Moved', moved)
-cv2.waitKey(0)
